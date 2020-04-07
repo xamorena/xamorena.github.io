3abe0a0bc01d883c7162091a7140039d10bd1192
Xavier <xavier.amorena@gmail.com>
2020-04-07 23:24:14 +0200

    Update

--- freeze.py
+++ freeze.py
@@ -5,6 +5,10 @@ if __name__ == '__main__':
     freezer = Freezer(application)
 
     @freezer.register_generator
+    def site_home():
+        yield "site.site_home", {}
+
+    @freezer.register_generator
     def site_topic():
         for topic in ['help', 'services', 'about', 'terms', 'privacy', 'teams']:
             yield "site.site_topic", {'topic': topic}
