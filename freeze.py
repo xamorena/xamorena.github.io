fe35f1ad29d2fa5ba7b7dfea1f78ebb2e1968abc
Xavier <xavier.amorena@gmail.com>
2020-04-07 21:54:34 +0200

    Update

--- freeze.py
+++ freeze.py
@@ -3,4 +3,4 @@ if __name__ == '__main__':
     from website.wsgi import application
     from flask_frozen import Freezer
     freezer = Freezer(application)
-    application.freezer.freeze()
+    freezer.freeze()
--- website/site.py
+++ website/site.py
@@ -34,17 +34,3 @@ def site_topic_page(topic, name):
     content, template = cm.get_content(f'{topic}_{name}', 'page.html')
     content['page']['path'] = f'/pages/{topic}/{name}'
     return render_template(template, **content)
-
-
-@site_bp.route("/citations")
-def site_citations():
-    cm = current_app.cms.get_content_manager()
-    context, template = cm.get_content("citations", 'citations.html')
-    return render_template(template, **context)
-
-
-@site_bp.route("/ajax/citations")
-def ajax_citations():
-    data = dict()
-    return jsonify(data)
-
